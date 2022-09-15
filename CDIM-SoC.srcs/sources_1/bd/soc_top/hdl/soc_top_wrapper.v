daf26037f2525dfba5838195ee00394683cb01eb
Yangyu Chen <cyy@cyyself.name>
2022-09-16 01:29:06 +0800

    add FAQ and make MII signals in a group

--- CDIM-SoC.srcs/sources_1/bd/soc_top/hdl/soc_top_wrapper.v
+++ CDIM-SoC.srcs/sources_1/bd/soc_top/hdl/soc_top_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
-//Date        : Fri Sep 16 00:19:30 2022
+//Date        : Fri Sep 16 00:53:37 2022
 //Host        : cyy-pc running 64-bit Debian GNU/Linux bookworm/sid
 //Command     : generate_target soc_top_wrapper.bd
 //Design      : soc_top_wrapper
@@ -26,20 +26,19 @@ module soc_top_wrapper
     DDR3_0_we_n,
     MDIO_0_mdc,
     MDIO_0_mdio_io,
+    MII_0_col,
+    MII_0_crs,
+    MII_0_rst_n,
+    MII_0_rx_clk,
+    MII_0_rx_dv,
+    MII_0_rx_er,
+    MII_0_rxd,
+    MII_0_tx_clk,
+    MII_0_tx_en,
+    MII_0_txd,
     UART_RX,
     UART_TX,
     clk,
-    mcoll_0,
-    mcrs_0,
-    mrxclk_0,
-    mrxd_0,
-    mrxdv_0,
-    mrxerr_0,
-    mtxclk_0,
-    mtxd_0,
-    mtxen_0,
-    mtxerr_0,
-    phy_rstn,
     resetn);
   output [12:0]DDR3_0_addr;
   output [2:0]DDR3_0_ba;
@@ -57,20 +56,19 @@ module soc_top_wrapper
   output DDR3_0_we_n;
   output MDIO_0_mdc;
   inout MDIO_0_mdio_io;
+  input MII_0_col;
+  input MII_0_crs;
+  output MII_0_rst_n;
+  input MII_0_rx_clk;
+  input MII_0_rx_dv;
+  input MII_0_rx_er;
+  input [3:0]MII_0_rxd;
+  input MII_0_tx_clk;
+  output MII_0_tx_en;
+  output [3:0]MII_0_txd;
   inout UART_RX;
   inout UART_TX;
   input clk;
-  input mcoll_0;
-  input mcrs_0;
-  input mrxclk_0;
-  input [3:0]mrxd_0;
-  input mrxdv_0;
-  input mrxerr_0;
-  input mtxclk_0;
-  output [3:0]mtxd_0;
-  output mtxen_0;
-  output [0:0]mtxerr_0;
-  output phy_rstn;
   input resetn;
 
   wire [12:0]DDR3_0_addr;
@@ -92,20 +90,19 @@ module soc_top_wrapper
   wire MDIO_0_mdio_io;
   wire MDIO_0_mdio_o;
   wire MDIO_0_mdio_t;
+  wire MII_0_col;
+  wire MII_0_crs;
+  wire MII_0_rst_n;
+  wire MII_0_rx_clk;
+  wire MII_0_rx_dv;
+  wire MII_0_rx_er;
+  wire [3:0]MII_0_rxd;
+  wire MII_0_tx_clk;
+  wire MII_0_tx_en;
+  wire [3:0]MII_0_txd;
   wire UART_RX;
   wire UART_TX;
   wire clk;
-  wire mcoll_0;
-  wire mcrs_0;
-  wire mrxclk_0;
-  wire [3:0]mrxd_0;
-  wire mrxdv_0;
-  wire mrxerr_0;
-  wire mtxclk_0;
-  wire [3:0]mtxd_0;
-  wire mtxen_0;
-  wire [0:0]mtxerr_0;
-  wire phy_rstn;
   wire resetn;
 
   IOBUF MDIO_0_mdio_iobuf
@@ -132,19 +129,18 @@ module soc_top_wrapper
         .MDIO_0_mdio_i(MDIO_0_mdio_i),
         .MDIO_0_mdio_o(MDIO_0_mdio_o),
         .MDIO_0_mdio_t(MDIO_0_mdio_t),
+        .MII_0_col(MII_0_col),
+        .MII_0_crs(MII_0_crs),
+        .MII_0_rst_n(MII_0_rst_n),
+        .MII_0_rx_clk(MII_0_rx_clk),
+        .MII_0_rx_dv(MII_0_rx_dv),
+        .MII_0_rx_er(MII_0_rx_er),
+        .MII_0_rxd(MII_0_rxd),
+        .MII_0_tx_clk(MII_0_tx_clk),
+        .MII_0_tx_en(MII_0_tx_en),
+        .MII_0_txd(MII_0_txd),
         .UART_RX(UART_RX),
         .UART_TX(UART_TX),
         .clk(clk),
-        .mcoll_0(mcoll_0),
-        .mcrs_0(mcrs_0),
-        .mrxclk_0(mrxclk_0),
-        .mrxd_0(mrxd_0),
-        .mrxdv_0(mrxdv_0),
-        .mrxerr_0(mrxerr_0),
-        .mtxclk_0(mtxclk_0),
-        .mtxd_0(mtxd_0),
-        .mtxen_0(mtxen_0),
-        .mtxerr_0(mtxerr_0),
-        .phy_rstn(phy_rstn),
         .resetn(resetn));
 endmodule
--- README.md
+++ README.md
@@ -23,3 +23,19 @@ NSCSCC Board (7a200t-2)
 [Linux](https://github.com/cyyself/linux/tree/cdim_soc)
 
 [uCore](https://github.com/cyyself/ucore-thumips/tree/cdim_soc)
+
+## FAQ
+
+- AXI Ethernet Link Down in Linux
+
+    If you program this soc and didn't press the reset from cold, the AXI Ethernet Lite in Linux will show the link down. Although the ethernet seems fine in U-Boot.
+
+    The solution is to press the reset button on the board to reset the ethernet phy and the whole system.
+
+- AXI Ethernet Lite transmit timeout in Linux
+
+    If the `Enable Asynchronous Clock operation` is enabled in AXI Interrupt Controller, the CPU will not receive any ethernet interrupt from AXI Interrupt Controller (Who can tell me why?).
+    
+    This problem can be checked by `cat /proc/interrupts` and check whether the eth0 interrupt count is zero.
+
+    If this option is enabled automatically by Vivado Block Design, you should manually disable this option.
